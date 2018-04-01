9bcedd7b047f0574d32c72ae8deebd3376408df7
Koutarou Tanaka <from.kyushu.island@gmail.com>
2018-04-02 01:00:20 +0900

    for koto params

--- src/config.js
+++ src/config.js
@@ -1,23 +1,25 @@
 /*
-config.js - Configuration for ZENCash Coin
+config.js - Configuration for Koto Coin
 */
 
 module.exports = {
   mainnet: {
-    messagePrefix: 'ZENCash main net',
+    messagePrefix: 'Koto main net',
     bip32: {
       public: '0488b21e',
       private: '0488ade4'
     },
-    pubKeyHash: '2089',
-    scriptHash: '2096',
-    zcPaymentAddressHash: '169a', // Private z-address
+    pubKeyHash: '1836',
+    scriptHash: '183b',
+    zcPaymentAddressHash: '16a8', // Private z-address
     zcSpendingKeyHash: 'ab36', // Spending key
     wif: '80'
   },
   testnet: {
-    wif: 'ef',
-    pubKeyHash: '2098',
-    scriptHash: '2092'
+    wif: '80',
+    pubKeyHash: '18a4',
+    scriptHash: '1839',
+    zcPaymentAddressHash: '16b6', // Private z-address
+    zcSpendingKeyHash: 'ac08' // Spending key
   }
 }
